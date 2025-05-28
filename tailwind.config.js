9c8196947e16ca6eeda72bc8cdfe458f64a8ff9c
SeokBong31 <han98571@naver.com>
2025-05-28 21:25:40 +0900

    Customize extra section in footer
    [1] Create 'postcss.config.js' & 'tailwind.config.js'.
    [2] Prepare 'assets\css\custom\input.css'.
    [3] Build Tailwind CSS.
    [4] Prepare 'layouts\partials\custom\footer.html'.
    [5] Remove public/ from Git tracking.
    
    (Ref. URL: https://github.com/imfing/hextra/discussions/682)

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    content: [
+        "./assets/**/*.css",
+        "./content/**/*.md",
+        "./layouts/**/*.html",
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
